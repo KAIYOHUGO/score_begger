6dcf3a857fe50a56c8daeab131000c1d41d891a7
KAIYOHUGO <41114603+KAIYOHUGO@users.noreply.github.com>
2025-02-18 22:49:50 +0800

    Create 43056.cpp

---
+++ itsa_oop/43056.cpp
@@ -0,0 +1,10 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdint.h>
+
+int main() {
+  char a, b, c, d;
+  scanf("%c%c%c%c", &a, &b, &c, &d);
+  printf("%c,%c,%c,%c\n", d, c, b, a);
+  return 0;
+}
